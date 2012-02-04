2d90b07e4aa810d5a4dcfa816d68edc8f6948df8
Saul Martinez <saul.martinez05@gmail.com>
2012-02-04 12:18:21 -0700

    sh404 SEF removed.

---
+++ media/sh404sef/sh404_upgrade_conf/mod_sh404sef_cpicon_modules_aztecasonora.samacs.local.php
@@ -0,0 +1,21 @@
+<?php // Extension params save file for sh404sef
+//    
+if (!defined('_JEXEC')) die('Direct Access to this location is not allowed.');
+$shConfig = array (
+  'title' => 'sh404sef control panel icon',
+  'note' => '',
+  'content' => '',
+  'ordering' => '0',
+  'position' => NULL,
+  'checked_out' => '0',
+  'checked_out_time' => '0000-00-00 00:00:00',
+  'publish_up' => '0000-00-00 00:00:00',
+  'publish_down' => '0000-00-00 00:00:00',
+  'published' => '0',
+  'module' => 'mod_sh404sef_cpicon',
+  'access' => '1',
+  'showtitle' => '1',
+  'params' => '',
+  'client_id' => '1',
+  'language' => '*',
+);
