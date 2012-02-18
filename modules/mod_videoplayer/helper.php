38b0e3a38bf20ce6cee0f9671505123b479fb329
Saul Martinez <saul.martinez05@gmail.com>
2012-02-18 11:52:51 -0700

    Removing debugging information.

--- modules/mod_videoplayer/helper.php
+++ modules/mod_videoplayer/helper.php
@@ -144,7 +144,6 @@ abstract class modVideoPlayerHelper {
 					$video->category = $article->category_title;
 					$video->parent = $article->parent_title;
 					if (empty($articles[$article->category_alias])) {
-						var_dump($article->category_alias);
 						$category = new stdClass();
 						$category->title = $article->category_title;
 						$category->id = (int)$article->catid;
