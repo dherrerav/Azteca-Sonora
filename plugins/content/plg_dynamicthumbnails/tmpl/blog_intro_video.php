fa8b74814b475adfc5c879b607599ab548cbd050
Saul Martinez <saul.martinez05@gmail.com>
2012-02-29 17:08:29 -0700

    Taking the first video preview.

--- plugins/content/plg_dynamicthumbnails/tmpl/blog_intro_video.php
+++ plugins/content/plg_dynamicthumbnails/tmpl/blog_intro_video.php
@@ -1,8 +1,5 @@
 <div class="article-images">
 	<div class="article-images-inner">
-		<? for ($i = 0; $i < count($videos); $i++) : ?>
 		<img src="<?= JURI::base() . $video->preview ?>" />
-		<? if ($i == 1) break; ?>
-		<? endfor ?>
 	</div>
 </div>
