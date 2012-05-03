3d7ff3d618fa478b3daced81c6a686261dfa861e
Saul Martínez <saul.martinez05@gmail.com>
2012-05-02 23:03:17 -0700

    Using video size for image.

--- plugins/content/plg_dynamicthumbnails/tmpl/article_video.php
+++ plugins/content/plg_dynamicthumbnails/tmpl/article_video.php
@@ -6,18 +6,22 @@ switch ($this->browser->getPlatform()) :
 	case Browser::PLATFORM_IPOD:
 		$video = $videos['m4v'];
 		$clipUrl = '\'' . JURI::base() . $video->source . '\'';
+		/*
 		$video->width = '100%';
 		$video->height = '100%';
+		*/
 		break;
 	case Browser::PLATFORM_ANDROID:
 		$video = $videos['flv'];
+		/*
 		$video->width = '100%';
 		$video->height = '100%';
+		*/
 		$clipUrl = '\'' . JURI::base() . $video->source . '\'';
 	default:
 		$video = $videos['flv'];
-		$video->width = $video->width . 'px';
-		$video->height = $video->height . 'px';
+		$video->width = $video->width;
+		$video->height = $video->height;
 		$clipUrl = 'flashembed.isSupported([9, 115]) ? ' .
 					'\'' . JURI::base() . $video->source . '\' : ' .
 					'\'' . JURI::base() . $videos['flv']->source . '\'';
@@ -27,7 +31,8 @@ endswitch;
 ?>
 <div class="article-videos">
 	<div class="article-videos-inner">
-		<div class="video" id="video-<?= $article->id ?>" style="width: <?= $video->width ?>; height: <?= $video->height ?>; background: transparent url(<?= JURI::base() . $video->preview ?>) no-repeat top left;">
+		<div class="video" id="video-<?= $article->id ?>">
+			<img src="<?= $video->preview ?>" width="<?= $video->width ?>" height="<?= $video->height ?>" alt="Reproducir" />
 		</div>
 	</div>
 </div>
