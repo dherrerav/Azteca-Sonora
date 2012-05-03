30580a74ebb59e399ffd0f9f4116ce1035b95f85
Saul Martínez <saul.martinez05@gmail.com>
2012-05-02 22:54:09 -0700

    Setting video format for Android devices.

--- plugins/content/plg_dynamicthumbnails/tmpl/article_video.php
+++ plugins/content/plg_dynamicthumbnails/tmpl/article_video.php
@@ -1,5 +1,5 @@
 <?php
-var_dump($this->browser->getPlatform());
+var_dump($this->browser->getPlatform(), Browser::PLATFORM_ANDROID);
 switch ($this->browser->getPlatform()) :
 	case Browser::PLATFORM_IPAD:
 	case Browser::PLATFORM_IPHONE:
@@ -10,29 +10,20 @@ switch ($this->browser->getPlatform()) :
 		$video->height = '100%';
 		break;
 	case Browser::PLATFORM_ANDROID:
+		$video = $videos['flv'];
+		$video->width = '100%';
+		$video->height = '100%';
+		$clipUrl = '\'' . JURI::base() . $video->source . '\'';
 	default:
-		var_dump($video);
-		if (Browser::PLATFORM_ANDROID) {
-			$video = $videos['flv'];
-			$video->width = $video->width . 'px';
-			$video->height = $video->height . 'px';
-			if (Browser::PLATFORM_ANDROID) {
-				$video->width = '100%';
-				$video->height = '100%';
-			}
-			$clipUrl = 'flashembed.isSupported([9, 115]) ? ' .
-						'\'' . JURI::base() . $video->source . '\' : ' .
-						'\'' . JURI::base() . $videos['flv']->source . '\'';
-		} else {
-			$video = $videos['flv'];
-			$video->width = $video->width . 'px';
-			$video->height = $video->height . 'px';
-			$clipUrl = 'flashembed.isSupported([9, 115]) ? ' .
-						'\'' . JURI::base() . $video->source . '\' : ' .
-						'\'' . JURI::base() . $videos['flv']->source . '\'';
-		}
+		$video = $videos['m4v'];
+		$video->width = $video->width . 'px';
+		$video->height = $video->height . 'px';
+		$clipUrl = 'flashembed.isSupported([9, 115]) ? ' .
+					'\'' . JURI::base() . $video->source . '\' : ' .
+					'\'' . JURI::base() . $videos['m4v']->source . '\'';
 		break;
 endswitch;
+
 ?>
 <div class="article-videos">
 	<div class="article-videos-inner">
